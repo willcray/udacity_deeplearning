da1ca75a5a142e81be9bfaecc97622d16dafdd7e
willcray <will.c.cray@gmail.com>
2019-01-27 17:30:44 -0700

    added all of the code from the assingment and worked through to problem 6. Ready to start implementing sklearn logistic regression model.

--- assignment1.py
+++ assignment1.py
@@ -12,7 +12,7 @@ from six.moves import cPickle as pickle
 
 url = 'https://commondatastorage.googleapis.com/books1000/'
 last_percent_reported = None
-data_root = '.'
+data_root = '.' # Change me to store data elsewhere
 
 def download_progress_hook(count, blockSize, totalSize):
   """A hook to report the progress of a download. This is mostly intended for users with
@@ -75,3 +75,190 @@ def maybe_extract(filename, force=False):
   
 train_folders = maybe_extract(train_filename)
 test_folders = maybe_extract(test_filename)
+
+image_size = 28  # Pixel width and height.
+pixel_depth = 255.0  # Number of levels per pixel.
+
+def load_letter(folder, min_num_images):
+  """Load the data for a single letter label."""
+  image_files = os.listdir(folder)
+  dataset = np.ndarray(shape=(len(image_files), image_size, image_size),
+                         dtype=np.float32)
+  print(folder)
+  num_images = 0
+  for image in image_files:
+    image_file = os.path.join(folder, image)
+    try:
+      image_data = (imageio.imread(image_file).astype(float) - 
+                    pixel_depth / 2) / pixel_depth
+      if image_data.shape != (image_size, image_size):
+        raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+      dataset[num_images, :, :] = image_data
+      num_images = num_images + 1
+    except (IOError, ValueError) as e:
+      print('Could not read:', image_file, ':', e, '- it\'s ok, skipping.')
+    
+  dataset = dataset[0:num_images, :, :]
+  if num_images < min_num_images:
+    raise Exception('Many fewer images than expected: %d < %d' %
+                    (num_images, min_num_images))
+    
+  print('Full dataset tensor:', dataset.shape)
+  print('Mean:', np.mean(dataset))
+  print('Standard deviation:', np.std(dataset))
+  return dataset
+        
+def maybe_pickle(data_folders, min_num_images_per_class, force=False):
+  dataset_names = []
+  for folder in data_folders:
+    set_filename = folder + '.pickle'
+    dataset_names.append(set_filename)
+    if os.path.exists(set_filename) and not force:
+      # You may override by setting force=True.
+      print('%s already present - Skipping pickling.' % set_filename)
+    else:
+      print('Pickling %s.' % set_filename)
+      dataset = load_letter(folder, min_num_images_per_class)
+      try:
+        with open(set_filename, 'wb') as f:
+          pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+      except Exception as e:
+        print('Unable to save data to', set_filename, ':', e)
+  
+  return dataset_names
+
+train_datasets = maybe_pickle(train_folders, 45000)
+test_datasets = maybe_pickle(test_folders, 1800)
+
+def make_arrays(nb_rows, img_size):
+  if nb_rows:
+    dataset = np.ndarray((nb_rows, img_size, img_size), dtype=np.float32)
+    labels = np.ndarray(nb_rows, dtype=np.int32)
+  else:
+    dataset, labels = None, None
+  return dataset, labels
+
+def merge_datasets(pickle_files, train_size, valid_size=0):
+  num_classes = len(pickle_files)
+  valid_dataset, valid_labels = make_arrays(valid_size, image_size)
+  train_dataset, train_labels = make_arrays(train_size, image_size)
+  vsize_per_class = valid_size // num_classes
+  tsize_per_class = train_size // num_classes
+    
+  start_v, start_t = 0, 0
+  end_v, end_t = vsize_per_class, tsize_per_class
+  end_l = vsize_per_class+tsize_per_class
+  for label, pickle_file in enumerate(pickle_files):       
+    try:
+      with open(pickle_file, 'rb') as f:
+        letter_set = pickle.load(f)
+        # let's shuffle the letters to have random validation and training set
+        np.random.shuffle(letter_set)
+        if valid_dataset is not None:
+          valid_letter = letter_set[:vsize_per_class, :, :]
+          valid_dataset[start_v:end_v, :, :] = valid_letter
+          valid_labels[start_v:end_v] = label
+          start_v += vsize_per_class
+          end_v += vsize_per_class
+                    
+        train_letter = letter_set[vsize_per_class:end_l, :, :]
+        train_dataset[start_t:end_t, :, :] = train_letter
+        train_labels[start_t:end_t] = label
+        start_t += tsize_per_class
+        end_t += tsize_per_class
+    except Exception as e:
+      print('Unable to process data from', pickle_file, ':', e)
+      raise
+    
+  return valid_dataset, valid_labels, train_dataset, train_labels
+            
+            
+train_size = 200000
+valid_size = 10000
+test_size = 10000
+
+valid_dataset, valid_labels, train_dataset, train_labels = merge_datasets(
+  train_datasets, train_size, valid_size)
+_, _, test_dataset, test_labels = merge_datasets(test_datasets, test_size)
+
+print('Training:', train_dataset.shape, train_labels.shape)
+print('Validation:', valid_dataset.shape, valid_labels.shape)
+print('Testing:', test_dataset.shape, test_labels.shape)
+
+def randomize(dataset, labels):
+  permutation = np.random.permutation(labels.shape[0])
+  shuffled_dataset = dataset[permutation,:,:]
+  shuffled_labels = labels[permutation]
+  return shuffled_dataset, shuffled_labels
+train_dataset, train_labels = randomize(train_dataset, train_labels)
+test_dataset, test_labels = randomize(test_dataset, test_labels)
+valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
+
+pickle_file = os.path.join(data_root, 'notMNIST.pickle')
+
+try:
+  f = open(pickle_file, 'wb')
+  save = {
+    'train_dataset': train_dataset,
+    'train_labels': train_labels,
+    'valid_dataset': valid_dataset,
+    'valid_labels': valid_labels,
+    'test_dataset': test_dataset,
+    'test_labels': test_labels,
+    }
+  pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+  f.close()
+except Exception as e:
+  print('Unable to save data to', pickle_file, ':', e)
+  raise
+
+statinfo = os.stat(pickle_file)
+print('Compressed pickle size:', statinfo.st_size)
+
+# Problem 1 - display samples of the images
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+for letter in letters:
+  path = "./notMNIST_small/" + letter + "/"
+  img = os.listdir(path)[0]
+  img_path = path + img
+  # get first file from directory and display it
+  print(img_path)
+  display(Image(img_path))
+
+# Problem 2 - display a sample of the labels and images from the ndarray
+for i in range(len(letters)): 
+  pickle_file = train_datasets[i]  # index 0 should be all As, 1 = all Bs, etc.
+  with open(pickle_file, 'rb') as f:
+      letter_set = pickle.load(f)  # unpickle
+      sample_idx = np.random.randint(len(letter_set))  # pick a random image index
+      sample_image = letter_set[sample_idx, :, :]  # extract a 2D slice
+      plt.figure()
+      plt.imshow(sample_image)  # display it
+  f.close()
+# Problem 3 - verify the classes are balanced post processing
+for letter in letters:
+  path = "./notMNIST_small/" + letter + "/"
+  print(letter + ": " + str(len(os.listdir(path))))
+
+# Problem 4 - check that the datasets are randomized
+# excluded for now
+"""
+with open(pickle_file, 'rb') as f:
+  pickle_dict = pickle.load(f)  # unpickle
+  print(len(pickle_dict))
+"""
+
+# Problem 5 - ensure there's no overlap in the training and validation datasets
+# excluded for now
+
+# Problem 6 - try an off the shelf classifier on the data - Logistical Regression
+"""
+logreg = LogisticRegression(C=1e5, solver='lbfgs', multi_class='multinomial')
+
+with open(pickle_file, 'rb') as f:
+  data = pickle.load(f)  # unpickle
+  indices = len(data)
+  for i in range (indices):
+    logreg.fit(train_dataset[i, :, :], train_labels)
+"""
