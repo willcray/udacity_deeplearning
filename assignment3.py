328b963f39a3649db84a4cc140854aaafb242650
willcray <william.c.cray@intel.com>
2019-02-08 16:35:40 -0700

    Fully completed assignment 3 by adding two more layers to the NN. Achieved 95.3% coveraged. Ready for more assignments.

--- assignment3.py
+++ assignment3.py
@@ -40,8 +40,11 @@ def accuracy(predictions, labels):
   return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
           / predictions.shape[0])
 
+
+"""
 batch_size = 128
-num_hidden_nodes = 1024
+num_hidden_nodes_1 = 1024
+num_hidden_nodes_2 = 512
 
 graph = tf.Graph()
 with graph.as_default():
@@ -56,35 +59,104 @@ with graph.as_default():
   
   # Variables.
   weights_1 = tf.Variable(
-    tf.truncated_normal([image_size * image_size, num_hidden_nodes]))
-  biases_1 = tf.Variable(tf.zeros([num_hidden_nodes]))
+    tf.truncated_normal([image_size * image_size, num_hidden_nodes_1]))
+  biases_1 = tf.Variable(tf.zeros([num_hidden_nodes_1]))
   
   weights_2 = tf.Variable(
-    tf.truncated_normal([num_hidden_nodes, num_labels]))
-  biases_2 = tf.Variable(tf.zeros([num_labels]))
+    tf.truncated_normal([num_hidden_nodes_1, num_hidden_nodes_2]))
+  biases_2 = tf.Variable(tf.zeros([num_hidden_nodes_2]))
   
+  weights_3 = (
+    tf.truncated_normal([num_hidden_nodes_2, num_labels]))
+  biases_3 = tf.Variable(tf.zeros([num_labels]))
+
   # Training computation.
   layer_1_out = tf.nn.relu(tf.matmul(tf_train_dataset, weights_1) + biases_1)
-  layer_1_out = tf.nn.dropout(layer_1_out, 0.5)
-  logits = tf.matmul(layer_1_out, weights_2) + biases_2
+  drop_1 = tf.nn.dropout(layer_1_out, 0.5)
+  layer_2_out = tf.nn.relu(tf.matmul(drop_1, weights_2) + biases_2)
+  drop_2 = tf.nn.dropout(layer_2_out, 0.5)
+  logits = tf.matmul(drop_2, weights_3) + biases_3
   loss = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits)
-        + 0.01 * tf.nn.l2_loss(weights_1) + 0.01 * tf.nn.l2_loss(weights_2))
+        + 0.01 * tf.nn.l2_loss(weights_1) + 0.01 * tf.nn.l2_loss(weights_2) + 0.01 * tf.nn.l2_loss(weights_3))
 
   # Optimizer.
   global_step = tf.Variable(0)
-  learning_rate = tf.train.exponential_decay(0.5, global_step, 500, 0.65)
+  learning_rate = tf.train.exponential_decay(0.5, global_step, 5000, 0.8)
+  optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+  
+  train_prediction = tf.nn.softmax(logits)
+
+  valid_pred_layer_1 = tf.nn.relu(tf.matmul(tf_valid_dataset, weights_1) + biases_1)
+  valid_pred_layer_2 = tf.nn.relu(tf.nn.softmax(tf.matmul(valid_pred_layer_1, weights_2) + biases_2))
+  valid_pred_layer_3 = tf.nn.softmax(tf.matmul(valid_pred_layer_2, weights_3) + biases_3)
+
+  test_pred_layer_1 = tf.nn.relu(tf.nn.relu(tf.matmul(tf_test_dataset, weights_1) + biases_1))
+  test_pred_layer_2 = tf.nn.relu(tf.nn.softmax(tf.matmul(test_pred_layer_1, weights_2) + biases_2))
+  test_pred_layer_3 = tf.nn.softmax(tf.matmul(test_pred_layer_2, weights_3) + biases_3)
+"""
+batch_size = 128
+num_hidden_nodes1 = 1024
+num_hidden_nodes2 = 512
+num_hidden_nodes3 = 256
+keep_prob = 0.5
+
+graph = tf.Graph()
+with graph.as_default():
+
+  # Input data. For the training data, we use a placeholder that will be fed
+  # at run time with a training minibatch.
+  tf_train_dataset = tf.placeholder(tf.float32,
+                                    shape=(batch_size, image_size * image_size))
+  tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+  tf_valid_dataset = tf.constant(valid_dataset)
+  tf_test_dataset = tf.constant(test_dataset)
+  global_step = tf.Variable(0)
+
+  # Variables.
+  weights1 = tf.Variable(
+    tf.truncated_normal(
+        [image_size * image_size, num_hidden_nodes1],
+        stddev=np.sqrt(2.0 / (image_size * image_size)))
+    )
+  biases1 = tf.Variable(tf.zeros([num_hidden_nodes1]))
+  weights2 = tf.Variable(
+    tf.truncated_normal([num_hidden_nodes1, num_hidden_nodes2], stddev=np.sqrt(2.0 / num_hidden_nodes1)))
+  biases2 = tf.Variable(tf.zeros([num_hidden_nodes2]))
+  weights3 = tf.Variable(
+    tf.truncated_normal([num_hidden_nodes2, num_hidden_nodes3], stddev=np.sqrt(2.0 / num_hidden_nodes2)))
+  biases3 = tf.Variable(tf.zeros([num_hidden_nodes3]))
+  weights4 = tf.Variable(
+    tf.truncated_normal([num_hidden_nodes3, num_labels], stddev=np.sqrt(2.0 / num_hidden_nodes3)))
+  biases4 = tf.Variable(tf.zeros([num_labels]))
+  
+  # Training computation.
+  lay1_train = tf.nn.relu(tf.matmul(tf_train_dataset, weights1) + biases1)
+  drop1 = tf.nn.dropout(lay1_train, 0.5)
+  lay2_train = tf.nn.relu(tf.matmul(drop1, weights2) + biases2)
+  drop2 = tf.nn.dropout(lay2_train, 0.5)
+  lay3_train = tf.nn.relu(tf.matmul(drop2, weights3) + biases3)
+  drop3 = tf.nn.dropout(lay3_train, 0.5)
+  logits = tf.matmul(drop3, weights4) + biases4
+  loss = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=tf_train_labels))
+    
+  # Optimizer.
+  learning_rate = tf.train.exponential_decay(0.5, global_step, 5000, 0.80, staircase=True)
   optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
   
   # Predictions for the training, validation, and test data.
   train_prediction = tf.nn.softmax(logits)
-  valid_pred_layer_1 = tf.nn.relu(tf.matmul(tf_valid_dataset, weights_1) + biases_1)
-  valid_pred_layer_2 = tf.nn.softmax(tf.matmul(valid_pred_layer_1, weights_2) + biases_2)
-  test_pred_layer_1 = tf.nn.relu(tf.matmul(tf_test_dataset, weights_1) + biases_1)
-  test_pred_layer_2 = tf.nn.softmax(tf.matmul(test_pred_layer_1, weights_2) + biases_2)
+  lay1_valid = tf.nn.relu(tf.matmul(tf_valid_dataset, weights1) + biases1)
+  lay2_valid = tf.nn.relu(tf.matmul(lay1_valid, weights2) + biases2)
+  lay3_valid = tf.nn.relu(tf.matmul(lay2_valid, weights3) + biases3)
+  valid_prediction = tf.nn.softmax(tf.matmul(lay3_valid, weights4) + biases4)
+  lay1_test = tf.nn.relu(tf.matmul(tf_test_dataset, weights1) + biases1)
+  lay2_test = tf.nn.relu(tf.matmul(lay1_test, weights2) + biases2)
+  lay3_test = tf.nn.relu(tf.matmul(lay2_test, weights3) + biases3)
+  test_prediction = tf.nn.softmax(tf.matmul(lay3_test, weights4) + biases4)
 
-
-num_steps = 10001
+num_steps = 20001
 # num_batches = 3
 
 
@@ -109,5 +181,5 @@ with tf.Session(graph=graph) as session:
       print("Minibatch loss at step %d: %f" % (step, l))
       print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
       print("Validation accuracy: %.1f%%" % accuracy(
-        valid_pred_layer_2.eval(), valid_labels))
-  print("Test accuracy: %.1f%%" % accuracy(test_pred_layer_2.eval(), test_labels))
+        valid_prediction.eval(), valid_labels))
+  print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
